594de325230b71bacc045ea6fc1578e230268da0
Sydorchuks <77363409+Sydorchuks@users.noreply.github.com>
2024-10-27 20:44:25 +0200

    Edited Nav component, added logo and set up for future website navigation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,19 @@
+"use client";
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href="/" className='flex gap-2 flex-center'>
+        <Image src="/assets/images/logo.svg" alt='logo' width={30} height={30} className='object-contain'/>
+        <p className='logo_text'> Share Prompts</p>
+      </Link>
+
+      {/** Mobile Navigation */}
+    </nav>
   )
 }
 
